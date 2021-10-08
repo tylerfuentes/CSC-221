e9b35c80034c8364cf09015b9a645447587391fd
tylerfuentes <66383167+tylerfuentes@users.noreply.github.com>
2021-10-08 18:57:30 -0400

    Add files via upload
    Newest installment for text based adventure.

---
+++ M3Lab1_TylerFuentes/M3Lab1_TylerFuentes.py
@@ -0,0 +1,116 @@
+# Text-based Adventure. 
+# This version has rooms which can be navigated and present descriptions on user intput.
+# CSC 221-M3Lab1 Linked Lists
+# 10/8/21
+# Tyler Fuentes
+
+# Imports elements from sister file "Room"
+from Room import Room
+# Allows for smoother passing of variables 
+class Game:
+    """
+    The Game class organizes all game data in a central location.
+    Usage:
+    - Set up game using your choice of room configurations
+      (TODO: Read these from a file in future)
+    - call loop()
+    """
+# Initializes variables to be utilized for text adventure
+    def __init__(self):
+        """ Initialize object (with no rooms) """
+        #self.player = player.Player()
+        self.rooms = { } # stored in dictionary
+        self.here = None # TODO: move this to Player
+        self.isPlaying = True
+        self.isVerbose = True # auto-look on move
+
+# Provides names and descriptions for rooms and places them into a dictionary
+    def setup(self):
+        """ setup(): create a graph of rooms for play. """
+        # just a test -- needs work
+        bedroom = Room( "Bedroom", 
+                   "This is an average bedroom.",
+                     { "north": "Bathroom",
+                     "south": "Living Room",
+                     "west" : "Kitchen",
+                     "east" : "Front Porch"} )
+        
+        livingRoom = Room ( "Living Room",
+                           "A TV, sofa, and game console are here.",
+                           { "north" : "Bedroom" } )
+        
+        bathroom = Room ( "Bathroom", 
+                         "There are wet towels hanging.",
+                         { "south" : "Bedroom" } )
+        
+        kitchen = Room ( "Kitchen", 
+                         "Although clean, there is a faint smell of burnt toast.",
+                         { "east" : "Bedroom" } )
+
+        porch = Room  ("Front Porch",
+                            "You look towards the driveway. You hear birds chirping in the distance.",
+                            { "west" : "Bedroom" })
+
+# Place rooms in a dictionary.
+        self.rooms = { bedroom.name: bedroom, 
+                    livingRoom.name: livingRoom,
+                    bathroom.name: bathroom,
+                    kitchen.name: kitchen,
+                    porch.name: porch}
+        
+        print(self.rooms)
+        self.here = bedroom # starting location
+
+# Loops program until user decides to quit    
+    def loop(self):
+        """ loop(): the main game loop.
+        Continues until the user quits. """
+        self.isPlaying = True
+        while self.isPlaying:
+            self.playerAction()
+        print("Game over, thanks for playing")
+
+# Drives commands to navigate through room objects
+    def playerAction(self):
+        """ Ask user for input, validate it, update the game state. """
+        command = input("Enter direction for travel or look for a description: >")
+        command = command.lower()
+        words = command.split() # split on whitespace
+        if len(words) < 1:
+            print("No input detected")
+            return
+# Provides effectiveness for travel by slicing the verb go and the direction       
+        verb = words[0]
+        if verb == "go":
+            direction = words[1]
+            # Can we go in the chosen direction from here?
+            if self.here.exits.get(direction) == None:
+                print("You can't go that way.")
+            else:   
+                # this key does exist
+                newRoomName = self.here.exits[direction]
+                newRoom     = self.rooms[newRoomName]
+                self.here   = newRoom
+                if self.isVerbose:
+                    self.here.describe()
+
+# Allows the user to view the room they are in and view options for travel
+        elif verb == "look":
+            self.here.look()
+# The program will end when the user types "quit"
+        elif verb == "quit":
+            self.isPlaying = False
+            print("quitting")
+        
+        else: # first word is verb
+            print("I don't know how to", words[0])
+
+# Organizes program functions 
+def main():
+    game = Game()
+    game.setup()
+    game.loop()
+    game.end()
+
+if __name__ == "__main__":
+    main()
---
+++ M3Lab1_TylerFuentes/Room.py
@@ -0,0 +1,93 @@
+# 10/1/# Text-based Adventure
+# CSC 221-M3T3 Linked Lists
+# 10/1/21
+# Tyler Fuentes
+
+
+class Room:
+    """
+    The Room class holds names, descriptions, and exits.
+    In future it should also manage objects in rooms, somehow
+    v3 - 10/4/21
+    """
+
+    def __init__(self):
+        """ Initialize an empty room"""
+        self.name = "Empty Room"
+        self.description = "No description"
+        self.exits = {}
+
+    def __init__(self, name, description, exits):
+        self.name = name
+        self.description = description
+        self.exits = exits
+
+    def __str__(self):
+        """ contains the name, description, and exits in a human-readable fashion"""
+        text = self.name + "\n"
+        text += self.description + "\n"
+        # append all exits
+        exitList = self.exits.keys() # this gives us a list of all directions ipresent in exits
+        for direction in exitList:
+            text += direction                     # North, South, etc. 
+            text += ": " + self.exits[direction]  # prints in format "North: Living Room", etc.
+            text += "\n"
+        return text
+
+    def describe(self):
+        """ print full room description. """
+        print(self.name)
+
+    def look(self):
+        """ prints the room description. """
+        # Appends all the exits
+        text = '\n'
+        text += "Exits" + "\n"
+        exitList = self.exits.keys() # this gives us a list of all directions present in exits
+        for direction in exitList:
+            text += direction                     # North, South, etc. 
+            text += ": " + self.exits[direction] + "\n"
+
+        print('\n',"Description: ", self.description, text, "\n", sep='')   
+
+    def exit(self, direction):
+        """
+        input: an exit direction, as string
+        output: a Room object - either the room the player
+            has moved to, or the current room if
+            movement failed.
+        """   
+        pass 
+                   
+            
+def main():
+    """
+    Currently used for testing.
+    TODO: uimplement doctests. """
+    bedroom = Room( "Bedroom", 
+                   "This is an average bedroom.",
+                   { "north": "Bathroom",
+                     "south": "Living Room",
+                     } )
+        
+    livingRoom = Room ( "Living Room",
+                       "A TV, sofa, and game console are here.",
+                       { "north" : "Bedroom" } )
+    
+    bathroom = Room ( "Bathroom", 
+                     "Somebody left the toothpaste uncapped again.",
+                     { "south" : "Bedroom" } )
+    
+    kitchen = Room ( "Kitchen", 
+                     "Smells like bacon despite a clean appearance.",
+                     { "west" : "Bedroom" } )
+
+    # Place rooms in a dictionary.
+    # (Game will handle this in the full version)
+    roomDict = { bedroom.name: bedroom, 
+                livingRoom.name: livingRoom,
+                bathroom.name: bathroom,
+                kitchen.name: kitchen}
+    
+if __name__ == "__main__":
+    main()
